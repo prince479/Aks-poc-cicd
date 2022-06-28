6ed5cb11bd2f3e8ed7e74a9050940088a8adc9a9
prince479 <ps441158@gmail.com>
2022-06-28 14:54:44 +0530

    firstcom

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestWEbApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
